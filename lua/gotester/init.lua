48fb09f69d26d5b97cd93cbb45f7cfad9ac8b650
Vladislav Panfilov <andromalak222@gmail.com>
2024-06-25 20:48:12 +0500

    toggle callback

--- lua/gotester/init.lua
+++ lua/gotester/init.lua
@@ -1,7 +1,34 @@
+local callback = function ()
+	print('TODO: run test here')
+end
+
 local M = {}
 
-M.setup = function (opts)
+M.setup = function(opts)
 	print('gotester setup')
+end
+
+M.toggle = function()
+	if (#vim.api.nvim_get_autocmds({ group = 'gotester' }) == 0) then
+		M.enable()
+	else
+		M.disable()
+	end
+end
+
+M.enable = function ()
+	vim.api.nvim_create_autocmd('BufWritePost', {
+		group = vim.api.nvim_create_augroup('gotester', { clear = true }),
+		callback = callback,
+	})
+end
+
+M.disable = function ()
+	vim.api.nvim_clear_autocmds({ group = 'gotester' })
+end
+
+M.test = function ()
+	print(#vim.api.nvim_get_autocmds({ group = 'gotester' }) == 0)
 end
 
 return M
